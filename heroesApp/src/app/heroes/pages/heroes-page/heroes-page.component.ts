1fe6d6490efc661ebe7db792a48f28235760dc95
Jorzon Olesky Delgado Orillo - TCS <jorzondelgado@bcp.com.pe>
2023-12-26 12:28:48 -0500

    new aplicattion heroeApp finish routes

---
+++ heroesApp/src/app/heroes/pages/heroes-page/heroes-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-page',
+  templateUrl: './heroes-page.component.html',
+  styles: [
+  ]
+})
+export class HeroesPageComponent {
+
+}
